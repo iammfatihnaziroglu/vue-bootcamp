e6253ac542a7ad3a38931ae2a28ab6c96771b141
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-23 18:39:50 +0300

    todo app

---
+++ ders2/2-todo-app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+      <h3 class="text-center">Todo App</h3>
+      <hr class="my-2" />
+      <label for="todoText"></label>
+      <input type="text" name="" id="todoText" placeholder="Type something..." >
+      <ul>
+        <li v-for="i in 5" :key="i" class="d-flex justify-content-between align-items-center" >
+          <span>Todo Item #{{ i }}  </span>
+          <button class="sm red" >Sil</button>
+        </li>
+      </ul>
+    </div>
+</template>
