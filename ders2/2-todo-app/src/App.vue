5d4f738b8eb7c6f7a95e5b966f89a56306a913c4
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-23 18:55:24 +0300

    sil button'u dinamikleştirildi

--- ders2/2-todo-app/src/App.vue
+++ ders2/2-todo-app/src/App.vue
@@ -5,10 +5,34 @@
       <label for="todoText"></label>
       <input type="text" name="" id="todoText" placeholder="Type something..." >
       <ul>
-        <li v-for="i in 5" :key="i" class="d-flex justify-content-between align-items-center" >
-          <span>Todo Item #{{ i }}  </span>
-          <button class="sm red" >Sil</button>
+        <li v-for="todoItem in todoList" :key="todoItem.id" class="d-flex justify-content-between align-items-center" >
+          <span>  {{ todoItem.text }}  </span>
+          <button @click="deleteItem(todoItem)" class="sm red" >Sil</button>
         </li>
       </ul>
+      <small class="d-flex justify-content-end mt-1 green" >5 adet todo app var. </small>
     </div>
 </template>
+
+<script>
+  export default{
+    data(){
+      return{
+        todoList: [
+          {id: 1, text: "Bootcamp Vue Ders #2.0"},
+          {id: 2, text: "Bootcamp Vue Ders #2.1"},
+          {id: 3, text: "Bootcamp Vue Ders #2.2"},
+          {id: 4, text: "Bootcamp Vue Ders #2.3"},
+          {id: 5, text: "Bootcamp Vue Ders #2.4"},
+          {id: 6, text: "Bootcamp Vue Ders #2.5"},
+          ],
+      };
+    },
+    methods:{
+      deleteItem(todoItem) {
+        console.log(todoItem);
+        this.todoList = this.todoList.filter((t) => t !== todoItem );
+      }
+    }
+  };
+</script>
