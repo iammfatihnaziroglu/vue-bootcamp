a388f80497e013f51b35a32d47ac6acbe4d3ec3c
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-23 19:10:44 +0300

    addNewTodo button eklendi

--- ders2/2-todo-app/src/App.vue
+++ ders2/2-todo-app/src/App.vue
@@ -3,14 +3,14 @@
       <h3 class="text-center">Todo App</h3>
       <hr class="my-2" />
       <label for="todoText"></label>
-      <input type="text" name="" id="todoText" placeholder="Type something..." >
+      <input @keydown.enter="addNewTodo" type="text" name="" id="todoText" placeholder="Type something..." >
       <ul>
         <li v-for="todoItem in todoList" :key="todoItem.id" class="d-flex justify-content-between align-items-center" >
           <span>  {{ todoItem.text }}  </span>
           <button @click="deleteItem(todoItem)" class="sm red" >Sil</button>
         </li>
       </ul>
-      <small class="d-flex justify-content-end mt-1 green" >5 adet todo app var. </small>
+      <small class="d-flex justify-content-end mt-1 green" >{{ todoList.length }} adet todo app var. </small>
     </div>
 </template>
 
@@ -30,9 +30,17 @@
     },
     methods:{
       deleteItem(todoItem) {
-        console.log(todoItem);
         this.todoList = this.todoList.filter((t) => t !== todoItem );
-      }
-    }
+      },
+      addNewTodo(event){
+      this.todoList.push({
+        id: new Date().getTime(),
+        text: event.target.value,
+      });
+      event.target.value = '';
+      console.log(event.target.value);
+    },
+    },
+    
   };
 </script>
--- ders2/2-todo-app/src/main.js
+++ ders2/2-todo-app/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "@/assets/style.css"
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount("#app");
