752fbee6732f923e87fa3aeecd0e1d1d1a823c4f
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-24 16:34:15 +0300

    components communications

--- ders3/0-components-communications/src/App.vue
+++ ders3/0-components-communications/src/App.vue
@@ -1,9 +1,32 @@
 <template>
   <div class="container text-center">
-    Container benim
+    <UserSection />
   </div>
 </template>
-
-<script>
-
+  
+  <script>
+import UserSection from "@/components/UserSection";
+export default {
+  components: {
+    UserSection
+  },
+  data() {
+    return {
+      provideData: {
+        userList: ["Fatih", "Mehmet", "Ankara", "Isparta"]
+      }
+    };
+  },
+  methods: {
+    newItem(item) {
+      this.provideData.userList.push(item);
+    }
+  },
+  provide() {
+    return {
+      userList: this.provideData.userList,
+      newItem: this.newItem
+    };
+  }
+};
 </script>
