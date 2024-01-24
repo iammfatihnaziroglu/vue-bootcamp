3d7ad1a6c1b28fcc4caf86a396781f8ac51d61bb
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-24 18:24:20 +0300

    components slots

---
+++ ders3/1-components-slots/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <ModalDialog :onSave="onSave" :title="'ModalDialog Title'" :content="content" /> -->
+  <ModalDialog :on-save="onSave" >
+    <template #title>
+      <h3>Slot ile Gelen Title Bilgisi</h3>
+    </template>
+
+    <template v-slot:content>
+      <p class="text-green">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis iusto nobis odit aliquid cupiditate, in beatae eum tempora rem id
+        ipsum aliquam, accusamus rerum numquam? Tempora optio itaque dicta!
+      </p>
+    </template>
+    <!-- <template #content>
+      <p class="text-green">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis iusto nobis odit aliquid cupiditate, in beatae eum tempora rem id
+        ipsum aliquam, accusamus rerum numquam? Tempora optio itaque dicta!
+      </p>
+    </template> -->
+
+    <template #default>
+      Bu default olarak gösterilecek..
+    </template>
+  </ModalDialog>
+</template>
+
+<script>
+import ModalDialog from "@/components/ModalDialog";
+export default {
+  components: {
+    ModalDialog
+  },
+  data() {
+    return {
+      content: `<h3 class="text-red">title</h3>`
+    };
+  },
+  methods: {
+    onSave() {
+      alert("Saved!!");
+    }
+  }
+};
+</script>
---
+++ ders3/1-components-slots/src/components/ModalDialog.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+      <div class="header">
+        <slot name="title" />
+      </div>
+      <hr />
+      <div class="content my-2">
+        <slot name="content" />
+      </div>
+      <slot />
+      <hr />
+      <div class="footer text-right">
+        <button @click="onSave" class="green">Kaydet</button>
+        <button class="mr-5">Kapat</button>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: ["title", "content", "onSave"]
+  };
+  </script>
