b93651d7146f17747a8ac3e4c6870b1ad58530eb
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-25 15:46:00 +0300

    request axios

--- ders3/3-http-request/src/App.vue
+++ ders3/3-http-request/src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="container">
-    <h3>Alışveriş Listesi</h3>
+    <h3 class="text-center" >Alışveriş Listesi</h3>
     <hr />
     <div class="my-2">
-      <input type="text" placeholder="Ne alacaksın?" @keydown.enter="onSave" />
+      <input type="text" class="text-center" placeholder="Ne Alacaksın?" @keydown.enter="onSave" />
     </div>
-    <ul>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 1</span>
-        <button class="sm red">Sil</button>
-      </li>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 2</span>
-        <button class="sm red">Sil</button>
-      </li>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 3</span>
-        <button class="sm red">Sil</button>
+    <ul v-if="itemsList.length > 0">
+      <li
+        v-for="item in itemsList"
+        :key="item.id"
+        class="d-flex justify-content-between align-items-center"
+      >
+        <span>{{ item.title }}</span>
+        <button @click="onDelete(item)" class="sm red">Sil</button>
       </li>
     </ul>
-    <small class="text-red d-flex justify-content-end align-items-center mt-1">5 adet alınacak ürün.</small>
+    <div v-else class="text-center">Herhangi bir ürün yok.</div>
+    <small
+      class="mt-2 text-red d-flex justify-content-end align-items-center"
+    >{{ itemCount }} adet alınacak ürün var.</small>
   </div>
 </template>
 
@@ -27,20 +26,41 @@
 import axios from "axios";
 export default {
   data() {
-    return {};
+    return {
+      itemsList: []
+    };
+  },
+  mounted() {
+    axios.get("http://localhost:3000/items").then(items_response => {
+      console.log("items_response :>> ", items_response);
+      this.itemsList = items_response.data || [];
+      console.log("this.itemsList :>> ", this.itemsList);
+    });
   },
   methods: {
     onSave(e) {
       const saveObject = {
         title: e.target.value,
-        create_at: new Date(),
-        compeleted: false
+        created_at: new Date(),
+        completed: false
       };
       axios.post("http://localhost:3000/items", saveObject).then(save_response => {
         console.log(save_response);
+        this.itemsList.push(save_response.data);
         e.target.value = "";
-        e.target.focus(); 
-      })
+        e.target.focus();
+      });
+    },
+    onDelete(item) {
+      axios.delete(`http://localhost:3000/items/${item.id}`).then(delete_response => {
+        console.log(delete_response);
+        this.itemsList = this.itemsList.filter(i => i.id !== item.id);
+      });
+    }
+  },
+  computed: {
+    itemCount() {
+      return this.itemsList.length || 0;
     }
   }
 };
