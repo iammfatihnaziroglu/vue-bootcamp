0597a6a4744d8ef7a054afa113e6794bfda66284
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-28 10:46:14 +0300

    router

--- ders5/1-vue-router-app/src/utils/appAxios.js
+++ ders5/1-vue-router-app/src/utils/appAxios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
+
 export const appAxios = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://localhost:3000",
   withCredentials: false,
-  headers: {
-    tokenX: "myToken",
-    "Content-Type": "application/json"
-  }
+  
 });
