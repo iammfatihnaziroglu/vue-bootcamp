52cf5d04a9519beab4cc1a3a132d5ecfb2a7c759
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2024-01-29 21:44:30 +0300

    authenticated control over route

--- ders6-socialmark-app/src/router/index.js
+++ ders6-socialmark-app/src/router/index.js
@@ -1,37 +1,43 @@
 import { createRouter, createWebHashHistory } from "vue-router";
+import store from "@/store";
 
 const routes = [
-    {
-        name: "HomePage",
-        path: "/",
-        component: () => import("@/views/Home.vue")
-    },
-    {
-        name: "LoginPage",
-        path: "/login",
-        component: () => import("@/views/Login.vue")
-    },
-    {
-        name: "RegisterPage",
-        path: "/register",
-        component: () => import("@/views/Register.vue")
-    },
-    {
-        name: "NewBookmarkPage",
-        path: "/newbookmark",
-        component: () => import("@/views/NewBookmark.vue")
-    },
-]
+  {
+    name: "HomePage",
+    path: "/",
+    component: () => import("@/views/Home.vue"),
+  },
+  {
+    name: "LoginPage",
+    path: "/login",
+    component: () => import("@/views/Login.vue"),
+  },
+  {
+    name: "RegisterPage",
+    path: "/register",
+    component: () => import("@/views/Register.vue"),
+  },
+  {
+    name: "NewBookmarkPage",
+    path: "/newbookmark",
+    component: () => import("@/views/NewBookmark.vue"),
+  },
+];
 
-const router = createRouter ({
-    routes,
-    history: createWebHashHistory()
-})
+const router = createRouter({
+  routes,
+  history: createWebHashHistory(),
+});
 
 router.beforeEach((to, from, next) => {
-    console.log("to =>", to)
-    console.log("from =>", from)
+  const authRequiredRoutes = ["HomePage"];
+  const _isAuthenticated = store.getters._isAuthenticated;
+  if (authRequiredRoutes.indexOf(to.name) > -1) {
+    if (_isAuthenticated) next();
+    else next({name: "LoginPage"});
+  } else {
     next();
-})
+  }
+});
 
 export default router;
